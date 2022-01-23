c6837138e53f1efd915466db43353c2bebe3c632
hotbreakb <areyhappy12@gmail.com>
2022-01-23 14:37:22 +0900

    Add ts for sortable

--- build/main.js
+++ build/main.js
@@ -1,15 +1,29 @@
 "use strict";
+// type text = string;
+// type img = string;
+// type video = string;
+// type todo = string;
+/*
+*
+* Vairables
+*
+*/
+const memoList = Array();
+const modal = document === null || document === void 0 ? void 0 : document.querySelector('.modal');
+const modalCloseBtn = modal === null || modal === void 0 ? void 0 : modal.querySelector('.close');
+const memoContainer = document.querySelector('.memo-container');
+const memos = document.querySelectorAll('.memo');
 /*
 *
 * Modal
 *
 */
-const modal = document.querySelector('.modal');
-const modalCloseBtn = modal === null || modal === void 0 ? void 0 : modal.querySelector('.close');
-let showModal = (memoType) => {
+let showModal = (memoType = "text") => {
     const contentName = modal === null || modal === void 0 ? void 0 : modal.querySelector("content-name");
-    switch (typeof memoType) {
-        case text:
+    switch (memoType) {
+        case "text": {
+            // contentName?.innerHTML = null;
+        }
     }
     if (modal != null)
         modal.classList.add('show');
@@ -17,5 +31,52 @@ let showModal = (memoType) => {
 let closeModal = () => {
     if (modal != null)
         modal.classList.remove('show');
+};
+let writeMemo = () => {
+    const memoTitle = modal === null || modal === void 0 ? void 0 : modal.querySelector('.memo-title');
+    const content = modal === null || modal === void 0 ? void 0 : modal.querySelector('.content');
+};
+/*
+*
+* Sortable memo
+*
+*/
+memos.forEach(memo => {
+    memo.addEventListener('dragstart', () => {
+        memo.classList.add('dragging');
+    });
+    memo.addEventListener('dragend', () => {
+        memo.classList.remove('dragging');
+    });
+});
+memoContainer === null || memoContainer === void 0 ? void 0 : memoContainer.addEventListener('dragover', (event) => {
+    const mouseEvent = event;
+    const dragging = memoContainer.querySelector('.dragging');
+    const { closestMemo, closestOffset } = getClosestMemo(memoContainer, mouseEvent.clientY);
+    if (dragging) {
+        if (closestOffset === Number.NEGATIVE_INFINITY) {
+            memoContainer.appendChild(dragging);
+        }
+        else {
+            memoContainer.insertBefore(dragging, closestMemo);
+        }
+    }
+});
+let getClosestMemo = (memoContainer, clientY) => {
+    const draggableMemos = [...memoContainer.querySelectorAll('.memo:not(.dragging')];
+    let closestOffset = Number.NEGATIVE_INFINITY;
+    return {
+        closestMemo: draggableMemos.reduce((closest, child) => {
+            const box = child.getBoundingClientRect();
+            const offset = clientY - box.top - box.height / 2;
+            if (offset < 0 && offset > closestOffset) {
+                closestOffset = offset;
+                return child;
+            }
+            else {
+                return closest;
+            }
+        }, draggableMemos[0]), closestOffset: closestOffset
+    };
 };
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ type memo = {
 
 /*
 *
-* Modal
+* Vairables
 *
 */
 
@@ -22,6 +22,13 @@ const memoList = Array<memo>();
 
 const modal = document?.querySelector('.modal');
 const modalCloseBtn = modal?.querySelector('.close');
+const memoContainer = document.querySelector('.memo-container');
+const memos = document.querySelectorAll('.memo');
+/*
+*
+* Modal
+*
+*/
 
 let showModal = (memoType: string = "text") => {
     const contentName = modal?.querySelector("content-name");
@@ -44,6 +51,54 @@ let closeModal = () => {
 let writeMemo = () => {
     const memoTitle = modal?.querySelector('.memo-title');
     const content = modal?.querySelector('.content');
-
-
 }
+
+/*
+*
+* Sortable memo
+*
+*/
+memos.forEach(memo => {
+    memo.addEventListener('dragstart', () => {
+        memo.classList.add('dragging');
+    });
+
+    memo.addEventListener('dragend', () => {
+        memo.classList.remove('dragging');
+    });
+});
+
+memoContainer?.addEventListener('dragover', (event) => {
+    const mouseEvent = event as MouseEvent;
+    const dragging = memoContainer.querySelector('.dragging');
+    const { closestMemo, closestOffset } = getClosestMemo(memoContainer, mouseEvent.clientY);
+
+    if (dragging) {
+        if (closestOffset === Number.NEGATIVE_INFINITY) {
+            memoContainer.appendChild(dragging);
+        } else {
+            memoContainer.insertBefore(dragging, closestMemo);
+        }
+    }
+});
+
+let getClosestMemo = (memoContainer: Element, clientY: number): { closestMemo: Element, closestOffset: number } => {
+    const draggableMemos = [...memoContainer.querySelectorAll('.memo:not(.dragging')];
+
+    let closestOffset = Number.NEGATIVE_INFINITY;
+
+    return {
+        closestMemo: draggableMemos.reduce((closest, child) => {
+            const box = child.getBoundingClientRect();
+            const offset = clientY - box.top - box.height / 2;
+
+            if (offset < 0 && offset > closestOffset) {
+                closestOffset = offset;
+                return child;
+            } else {
+                return closest;
+            }
+
+        }, draggableMemos[0]), closestOffset: closestOffset
+    };
+};
